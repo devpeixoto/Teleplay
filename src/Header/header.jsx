cd2e40cca9332c7f2411465d2b14d234f3d115ee
Gabriel <peixotogp@hotmail.com>
2023-07-21 07:39:20 -0300

    first commit test

---
+++ src/Header/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className=" flex justify-center text-white" >Olá Mundo</header>
+    )
+}
+
+export default Header;
